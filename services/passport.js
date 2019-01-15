fe669c395c850eaeed539122a2bbf0ad4937010c
chaoyuquenet <chaoyu1979@gmail.com>
2019-01-15 11:41:36 +0100

    node react

--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,10 @@ const keys = require('../config/keys');
 
 const user = mongoose.model('user');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -18,9 +22,12 @@ passport.use(
         .then(existingUser => {
           if (existingUser) {
             // we already have a record with the given googleId/profile id
+            done(null, existingUser);
           } else {
             // we don't have a user record with this ID, make a new record
-            new user({ googleId: profile.id }).save();
+            new user({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
           }
         });
     }
